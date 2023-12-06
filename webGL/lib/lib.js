f9d824fcd14b8fe19fcfee75f34201d41c2b1b6c
xianren <2958197921@qq.com>
2023-12-06 21:32:59 +0800

    update

--- webGL/lib/lib.js
+++ webGL/lib/lib.js
@@ -51,3 +51,59 @@ export function getColor() {
         a: Math.random()
     }
 }
+
+/**
+ * 绘制圆
+ * @param {{x:number,y:number}} point 圆心坐标
+ * @param {number} radius 半径长度
+ * @param {number} nums 由多少个三角形组成
+ * @return {Array<number>} 顶点坐标
+ */
+export function drawCircle(point, radius, nums) {
+    // 顶点数组
+    const points = [point.x, point.y]
+    for (let i = 0; i <= nums; i++) {
+        // 每个坐标点与x轴的夹角
+        let angle = i * Math.PI * 2 / nums
+        // 计算点的横纵坐标
+        let pointx = point.x + Math.cos(angle) * radius
+        let pointy = point.y + Math.sin(angle) * radius
+        points.push(pointx, pointy)
+    }
+    return points
+}
+
+/**
+ * 绘制环
+ * @param {{x:number,y:number}} point 圆心坐标
+ * @param {number} inner 小圆半径
+ * @param {number} outer 大圆半径
+ * @param {number} nums 由多少个三角形组成
+ * @return {Array<number>} 顶点坐标
+ */
+export function drawRing(point, inner, outer, nums) {
+    // 顶点坐标
+    const points = []
+    // 索引数组
+    const indexs = []
+    for(let i = 0; i <= nums; i++){
+        // 每个坐标点与x轴的夹角
+        let angle = i * Math.PI * 2 / nums
+        // 计算点的横纵坐标
+        let innerx = point.x + Math.cos(angle) * inner
+        let innery = point.y + Math.sin(angle) * inner
+        let outerx = point.x + Math.cos(angle) * outer
+        let outery = point.y + Math.sin(angle) * outer
+        points.push(innerx, innery, outerx, outery)
+        let v0 = i * 2
+        let v1 = v0 + 1
+        let v2 = v1 + 1
+        let v3 = v2 + 1
+        if (i == nums){
+            v2 = 0
+            v3 = 1
+        }
+        indexs.push(v0, v1, v2, v3)
+    }
+    return {points, indexs}
+}
